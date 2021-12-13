6aa2837f48c4a82a1e1356f569344f6315823623
StefanosTouf <stefetoufe@gmail.com>
2021-12-13 22:26:56 +0000

    count

--- logger.sh
+++ logger.sh
@@ -4,9 +4,12 @@ prefixes[0]=Error
 prefixes[1]=Warning
 prefixes[2]=Info
 
+cnt=0
+
 while true
 do 
     ran_num=$((0 + $RANDOM % 3))
-    echo "${prefixes[ran_num]}: I am logging, theres no stopping"
+    echo "${prefixes[ran_num]}: I am logging, theres no stopping -- ${cnt}"
+    cnt=$((cnt + 1))
     sleep 1
 done
